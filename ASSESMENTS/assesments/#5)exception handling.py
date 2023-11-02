6078f924f4701dcd605b4aa2590bda3b2df24d98
saiinfo <developer.saiinfo@gmail.com>
2023-11-02 15:41:41 +0530

    Delete ASSESMENTS directory

--- ASSESMENTS/assesments/#5)exception handling.py
+++
@@ -1,5 +0,0 @@
-try:
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-except ValueError:
-    print("Invalid input. Please enter a numerical value.")
--- ASSESMENTS/assesments/screen capture.py
+++
@@ -1,4 +0,0 @@
-import pyautogui
-
-myScreenshot = pyautogui.screenshot()
-myScreenshot.save(r'C:\Users\vigne\Desktop\WORK\screenshot_1.png')
