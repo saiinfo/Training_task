3cb169687e1b00f50949129551281145d96ca1c0
Hrushikesh Nayak <96150298+hrushikeshnayak@users.noreply.github.com>
2023-11-03 17:37:55 +0530

    Delete Hrushikesh nayak/assignment 5/15. media player django/media directory

--- Hrushikesh nayak/assignment 5/15. media player django/media/media/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from media_player import views
- 
-urlpatterns = [
-    path('' , views.index , name = 'home'),
-    path('clock' , views.clock , name = 'clock'),
-    path('services' , views.services , name = 'services'),
-]
