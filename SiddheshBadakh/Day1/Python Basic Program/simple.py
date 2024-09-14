b7e96a81261ae7455315875cebe6001e43bc2665
saiinfo <developer.saiinfo@gmail.com>
2024-09-14 17:02:56 +0530

    2/09/2024

---
+++ SiddheshBadakh/Day1/Python Basic Program/simple.py
@@ -0,0 +1,12 @@
+num1=10
+num2=20
+
+add=num1+num2
+sub=num2-num1
+mul=num1*num2
+div=num2/num1
+
+print(add)
+print(sub)
+print(mul)
+print(div)
---
+++ SiddheshBadakh/Day1/Python Basic Program/swap.py
@@ -0,0 +1,10 @@
+num1=10
+num2=20
+
+
+num1=num1+num2
+num2=num1-num2
+num1=num1-num2
+
+print(num1)
+print(num2)
